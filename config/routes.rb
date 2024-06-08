57db983170578d0dcc07ed25c9877c537ad3dc0e
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2024-06-08 14:24:30 +0530

    Added API based architecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tasks, only: :index
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  get "*path", to: "home#index", via: :all
 end
