bf18c4a2b7344288939eb0ccd3fbdb121e3a9934
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2024-06-08 13:34:09 +0530

    Setup Tailwind CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,4 @@
 //
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
+import "../stylesheets/application.scss"
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  important: true,
+  content: ["./app/javascript/**/*.{js,jsx}", "./app/views/**/*.html.erb"],
+};
